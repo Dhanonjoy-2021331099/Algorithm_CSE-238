4f099f0699533db5669484998b68575060808399
Dhanonjoy-2021331099 <138089152+Dhanonjoy-2021331099@users.noreply.github.com>
2024-05-23 11:41:46 +0600

    Update 8 puzzle( Heuristic ).cpp

--- 8 puzzle( Heuristic ).cpp
+++ 8 puzzle( Heuristic ).cpp
@@ -1,86 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> x; // Global array to store positions of queens
 
-// matrix print
-void Disply(const array<int, 9>& matrix) {
-    for (int i = 0; i < 9; ++i) {
-        if (i % 3 == 0 && i > 0) {
-            cout << endl;
+bool Place(int k, int i) {
+    for (int j = 1; j < k; ++j) {
+        if (x[j] == i || abs(x[j] - i) == abs(j - k)) {
+            return false;
         }
-        cout << matrix[i] << " ";
+    }
+    return true;
+}
+
+void printSolution(int n) {
+    char board[n][n];
+    // Initialize the board with '*'
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            board[i][j] = '*';
+        }
+    }
+    // Place the queens
+    for (int i = 1; i <= n; ++i) {
+        board[i - 1][x[i] - 1] = 'Q';
+    }
+    // Print the board
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
     }
     cout << endl;
 }
 
-// Heuristic Value Count
-int count(const array<int, 9>& s) {
-    int c = 0;
-    array<int, 9> ideal = {1, 2, 3, 4, 5, 6, 7, 8, 0};
-
-    for (int i = 0; i < 9; ++i) {
-        if (s[i] != 0 && s[i] != ideal[i]) {
-            c++;
+void NQueens(int k, int n) {
+    for (int i = 1; i <= n; ++i) {
+        if (Place(k, i)) {
+            x[k] = i;
+            if (k == n) {
+                printSolution(n);
+            } else {
+                NQueens(k + 1, n);
+            }
         }
     }
-    return c;
-}
-
-pair<array<int, 9>, int>
-move(const vector<int>& ar,  int p,  const array<int, 9>& st) {
-    int rh = INT_MAX;
-    array<int, 9> store_st = st;
-
-    for (int i : ar) {
-        array<int, 9> dupl_st = st;
-
-        swap(dupl_st[p] , dupl_st[i]);
-
-        int trh = count(dupl_st);
-
-        if (trh < rh) {
-            rh = trh;
-            store_st = dupl_st;
-        }
-    }
-    return make_pair(store_st, rh);
 }
 
 int main() {
-    array<int, 9> state = {1, 2, 3, 0, 4, 6, 7, 5, 8};
-    int h = count(state);
-    int Level = 0;
-
-    cout << "Distance : " << Level <<endl;
-    Disply(state);
-    cout << "Heuristic Value(Misplaced) : " << h << endl;
-
-    while (h >0 ) {
-        int pos = find(state.begin(), state.end(), 0) - state.begin();
-        Level++;
-        vector<int> arr;
-
-        switch (pos) {
-            case 0: arr = {1, 3}; break;
-            case 1: arr = {0, 2, 4}; break;
-            case 2: arr = {1, 5}; break;
-            case 3: arr = {0, 4, 6}; break;
-            case 4: arr = {1, 3, 5, 7}; break;
-            case 5: arr = {2, 4, 8}; break;
-            case 6: arr = {3, 7}; break;
-            case 7: arr = {4, 6, 8}; break;
-            case 8: arr = {5, 6}; break;
-        }
-
-        tie(state, h) = move(arr, pos, state);
-
-        cout << "\n\nDistance : " << Level << endl;
-        Disply(state);
-        cout << "Heuristic Value(Misplaced) : " << h << endl;
-
-        if(h==0) cout << "\nFinally Initial State = Goal State.\n";
-    }
-
+    int n;
+    cout << "Enter the number of queens: ";
+    cin >> n;
+    x.resize(n + 1); // Resize x to hold positions for n queens
+    NQueens(1, n);
     return 0;
 }
-
